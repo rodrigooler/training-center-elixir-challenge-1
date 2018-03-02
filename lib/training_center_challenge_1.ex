0c25227ac4d8a322dfebc4640e5e90e1c8729cef
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-03-01 22:24:52 -0300

    create project

---
+++ lib/training_center_challenge_1.ex
@@ -0,0 +1,18 @@
+defmodule TrainingCenterChallenge1 do
+  @moduledoc """
+  Documentation for TrainingCenterChallenge1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrainingCenterChallenge1.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/training_center_challenge_1_test.exs
@@ -0,0 +1,8 @@
+defmodule TrainingCenterChallenge1Test do
+  use ExUnit.Case
+  doctest TrainingCenterChallenge1
+
+  test "greets the world" do
+    assert TrainingCenterChallenge1.hello() == :world
+  end
+end
